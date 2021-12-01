2e4a43cfcf3456754e41f8c8c296e81fd04fbc68
Kenan Haskić <kenan.haskic@green-river-media.com>
2021-12-01 17:00:32 +0100

    Monsters rolodex final version

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
+
+class App extends Component {
+  state = {
+    monsters: [],
+    searchField:''
+  }
+
+   componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=> response.json())
+    .then(response=>this.setState({
+        monsters:response
+    }))
+  }
+ handleChange = (e) =>{
+  this.setState({searchField: e.target.value})
+ }
+
+
+
+  render(){
+    const {monsters,searchField} = this.state;
+    const filteredMonsters=monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
+    return (
+    <div className="App">
+      <h1>Monsters Rolodex</h1>
+    <SearchBox placeholder="Search monsters" handleChange={this.handleChange}/>
+       <CardList monsters={ filteredMonsters}/>   
+    </div>
+  );
+}
+}
+
+export default App;
